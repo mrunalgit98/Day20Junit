616333cdb2c971dd4faad82fae0d3cc6bba1ebde
mrunal <mrunalbhoir100@gmail.com>
2022-07-30 11:53:58 +0530

    UC3 EMAIL

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,8 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User{
-    public boolean Lastname(String str) {
-        String regex="[A-Z]{1}[a-z]{2,}";
+    public boolean email(String str) {
+        String regex="[a-zA-Z0-9.]+[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.][a-z]*";
         Pattern p=Pattern.compile(regex);
         Matcher mat=p.matcher(str);
         boolean result=mat.find();
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
     User m=new User();
     @Test
     public void Test() {
-        boolean result =m.Lastname("Aua");
+        boolean result =m.email("abc.xyz@bl.co.in");
         assertEquals(result,true);
 
     }
