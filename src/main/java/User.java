a921599f5271349b7fc69cc73043dde0acda964c
mrunal <mrunalbhoir100@gmail.com>
2022-07-30 11:50:12 +0530

    UC2 LAST NAME

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User{
-    public boolean Firstname(String str) {
+    public boolean Lastname(String str) {
         String regex="[A-Z]{1}[a-z]{2,}";
         Pattern p=Pattern.compile(regex);
         Matcher mat=p.matcher(str);
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
     User m=new User();
     @Test
     public void Test() {
-        boolean result =m.Firstname("Aua");
+        boolean result =m.Lastname("Aua");
         assertEquals(result,true);
 
     }
