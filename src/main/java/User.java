865861559b01e9d6e48520a6bf742b936ffed1b3
mrunal <mrunalbhoir100@gmail.com>
2022-07-30 12:34:44 +0530

    UC9 RULE 4

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,8 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User{
-    public boolean Rule3(String str) {
-        String regex="[A-Z]+[0-9][a-zA-Z0-9!@#$%]{6}";
+    public boolean Rule4(String str) {
+        String regex="[A-Z]+[0-9]+[~!@#$%^&*]{1}[a-zA-Z0-9!@#$%]{5}";
         Pattern p=Pattern.compile(regex);
         Matcher mat=p.matcher(str);
         boolean result=mat.find();
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
     User m=new User();
     @Test
     public void Test() {
-        boolean result =m.Rule3("A1bcdefghijkl");
+        boolean result =m.Rule4("A1@bcdefghijkl");
         assertEquals(result,true);
 
     }
