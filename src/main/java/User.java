f3ffa07b82e5512981dc586d3584572443a2014a
mrunal <mrunalbhoir100@gmail.com>
2022-07-30 12:32:44 +0530

    UC7 RULE 3

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,8 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User{
-    public boolean Rule2(String str) {
-        String regex="[A-Z]+[a-zA-Z0-9!@#$%]{7}";
+    public boolean Rule3(String str) {
+        String regex="[A-Z]+[0-9][a-zA-Z0-9!@#$%]{6}";
         Pattern p=Pattern.compile(regex);
         Matcher mat=p.matcher(str);
         boolean result=mat.find();
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
     User m=new User();
     @Test
     public void Test() {
-        boolean result =m.Rule2("Abcdefghijkl");
+        boolean result =m.Rule3("A1bcdefghijkl");
         assertEquals(result,true);
 
     }
