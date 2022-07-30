77b3842ae2b5fbe412f9db43729345f233d43af1
mrunal <mrunalbhoir100@gmail.com>
2022-07-30 14:07:04 +0530

    UC9 Email

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,52 +1,46 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class User{
+public class User {
     public boolean Firstname(String str) {
-        String regex="[A-Z]{1}[a-z]{2,}";
-        Pattern p=Pattern.compile(regex);
-        Matcher mat=p.matcher(str);
-        boolean result=mat.find();
+        String regex = "[A-Z]{1}[a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher mat = p.matcher(str);
+        boolean result = mat.find();
         return result;
     }
 
     public boolean Lastname(String str) {
-        String regex="[A-Z]{1}[a-z]{2,}";
-        Pattern p=Pattern.compile(regex);
-        Matcher mat=p.matcher(str);
-        boolean result=mat.find();
+        String regex = "[A-Z]{1}[a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher mat = p.matcher(str);
+        boolean result = mat.find();
         return result;
     }
 
     public boolean email(String str) {
-        String regex="[a-zA-Z0-9.]+[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.][a-z]*";
-        Pattern p=Pattern.compile(regex);
-        Matcher mat=p.matcher(str);
-        boolean result=mat.find();
+        String regex = "[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$";
+        Pattern p = Pattern.compile(regex);
+        Matcher mat = p.matcher(str);
+        boolean result = mat.find();
         return result;
     }
 
     public boolean MobileNo(String str) {
-        String regex="(91-)[0-9]{10}$";
-        Pattern p=Pattern.compile(regex);
-        Matcher mat=p.matcher(str);
-        boolean result=mat.find();
-        return result;
-    }
-    public boolean Password(String str){
-        String regex="[A-Z]+[0-9]+[~!@#$%^&*]{1}[a-zA-Z0-9!@#$%]{5}";
-        Pattern p=Pattern.compile(regex);
-        Matcher mat=p.matcher(str);
-        boolean result=mat.find();
+        String regex = "(91-)[0-9]{10}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher mat = p.matcher(str);
+        boolean result = mat.find();
         return result;
     }
 
-    public String moodAnalyzer(String firstname,String lastname,String mail,String mobileNo,String password){
-        if(Firstname(firstname)==true && Lastname(lastname)==true && email(mail)==true && MobileNo(mobileNo)==true && Password(password)==true  ){
-
-        return "Happy";
-    }else {
-            return "Sad";
-        }
+    public boolean Password(String str) {
+        String regex = "[A-Z]+[0-9]+[~!@#$%^&*]{1}[a-zA-Z0-9!@#$%]{5}";
+        Pattern p = Pattern.compile(regex);
+        Matcher mat = p.matcher(str);
+        boolean result = mat.find();
+        return result;
     }
-}
+
+
+    }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -2,92 +2,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class UserTest {
     User m = new User();
 
-    @Test
-    public void Test() {
-        boolean result = m.Firstname("Mrunal");
-        assertEquals(result, true);
+    String email;
+    boolean expResult;
 
+    public UserTest(String email,boolean expResult){
+        this.email=email;
+        this.expResult=expResult;
     }
-
-    @Test
-    public void TestFalse() {
-        boolean result = m.Firstname("mrunal");
-        assertEquals(result, false);
-
+    @Parameterized.Parameters
+    public static Collection input(){
+        return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
+                { "abc.100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
+                { "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
+                { "abc+100@gmail.com", true } });
     }
-
-
     @Test
-    public void Test1() {
-        boolean result = m.Lastname("Bhoir");
-        assertEquals(result, true);
-    }
-
-    @Test
-    public void Test1False() {
-        boolean result = m.Lastname("bhoir");
-        assertEquals(result, false);
-    }
-
-
-
-    @Test
-    public void Test3() {
-        boolean result =m.email("abc.xyz@bl.co.in");
-        assertEquals(result,true);
-
-    }
-
-
-    @Test
-    public void Test3False() {
-        boolean result =m.email("abc.xyz.bl.co.in");
-        assertEquals(result,false);
-
-    }
-
-    @Test
-    public void Test4() {
-        boolean result =m.MobileNo("91-9892820222");
-        assertEquals(result,true);
-
-    }
-
-    @Test
-    public void Test4False() {
-        boolean result =m.MobileNo("91-989282022243");
-        assertEquals(result,false);
-
-    }
-
-    @Test
-    public void Test5(){
-        boolean result=m.Password("A1@aamsmads");
-        assertEquals(result,true);
-    }
-
-    @Test
-    public void Test5False(){
-        boolean result=m.Password("A1aamsmads");
-        assertEquals(result,false);
-    }
-
-    public void  moodanalyserTest_happy(){
+    public void testEmail(){
         User user=new User();
-        String isHappy=user.moodAnalyzer("Mrunal","Bhoir","abc.xyz@bl.co.in","91-9892820222","A1@aamsmads");
-        Assert.assertEquals("Happy",isHappy);
-
-      }
-
-      public void moodanalyserTest_sad(){
-        User user=new User();
-        String isSad=user.moodAnalyzer("mrunal","bhoir","abc.xyz.bl.co.in","91-989282022243","A1aamsmads");
-        Assert.assertEquals("Sad",isSad);
+        Assert.assertEquals(expResult,user.email(email));
     }
-    }
-
+}
